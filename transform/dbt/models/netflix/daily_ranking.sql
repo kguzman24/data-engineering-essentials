2fdd913dff97b5ec49e40b8c0dda708baa45c4af
Karen Guzman <krngzmn10@gmail.com>
2025-09-15 16:27:46 -0400

    one SQL line

--- transform/dbt/models/netflix/daily_ranking.sql
+++ transform/dbt/models/netflix/daily_ranking.sql
@@ -5,3 +5,9 @@
  )}}
 
 -- SQL GOES HERE
+
+SELECT
+    "As of" date_label, Rank, Title, Type
+    FROM 'https://s3.amazonaws.com/uvads-systems/data/netflix_daily_top_10.parquet'
+    WHERE Rank = 1
+    ORDER BY date_label DESC
